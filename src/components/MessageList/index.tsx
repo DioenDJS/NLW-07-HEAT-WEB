6c7aa8c4f2f12e5b7566b7136e0e4e37911ececd
DioenDJS <negociospacheti@gmail.com>
2022-02-02 13:37:09 -0300

    :art: add component message list

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,50 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/assets/logo.svg';
+
+export function MessageList() {
+    return(
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o 
+                        melhor de todos os tempos. vamos pra cima!
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com//DioenDJS.png" alt="Image User" />
+                        </div>
+                        <span>Diovane Luis</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o 
+                        melhor de todos os tempos. vamos pra cima!
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com//DioenDJS.png" alt="Image User" />
+                        </div>
+                        <span>Diovane Luis</span>
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o 
+                        melhor de todos os tempos. vamos pra cima!
+                    </p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com//DioenDJS.png" alt="Image User" />
+                        </div>
+                        <span>Diovane Luis</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
